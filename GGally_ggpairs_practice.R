4bdc6ebd06b7beaf034e3e44d738f2568aa5d496
Delaney <112586752+deedavis06@users.noreply.github.com>
2023-01-07 18:13:26 -0600

    Add files via upload

---
+++ GGally_ggpairs_practice.R
@@ -0,0 +1,12 @@
+library(datasets)
+library(GGally)
+data(iris)
+
+#Make a matrix of plots using the iris dataset
+#the colour refers to different colours by group(the variable Species)
+ggpairs(iris, aes(colour=Species))
+
+#make the text smaller for the correlation values
+ggpairs(iris, aes(colour=Species), upper = list(continuous = wrap("cor", size = 2.5)))
+
+
